6302f907d1ce2d62eafef16cf8095dbac94cd105
김혜진 <khj000217@naver.com>
2026-08-11 16:16:46 +0900

    docs: first commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    visualizer({
+      open: true, // 빌드 후 자동으로 브라우저에서 결과 열기
+      filename: 'stats.html', // 시각화 결과를 저장할 파일 이름
+      template: 'treemap', // 시각화 템플릿 (treemap, sunburst, network 등)
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
+});
