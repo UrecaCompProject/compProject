3f38145cc28b8610125069e30330f332b3253765
김혜진 <khj000217@naver.com>
2026-08-20 17:15:01 +0900

    fix: supabase 클라이언트 sdk 설치해서 오류 수정

--- src/features/shared/components/Line.tsx
+++ src/features/shared/components/Line.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Line() {
   return <div className="w-full h-px bg-border"></div>;
 }
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,8 @@
 import { Outlet, useLocation } from 'react-router';
-import { Header, Navbar, Footer } from '@/layout';
+
+import Footer from './Footer';
+import Header from './Header';
+import Navbar from './Navbar';
 
 export default function Layout() {
   const { pathname } = useLocation();
