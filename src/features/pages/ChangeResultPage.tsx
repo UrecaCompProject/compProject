2d7b5f6464f448409a9aa8cfb9435348603726fa
김혜진 <khj000217@naver.com>
2026-08-20 16:17:10 +0900

    chore: 사용하지 않는 변수 주석처리

--- src/features/pages/ChangeResultPage.tsx
+++ src/features/pages/ChangeResultPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 
 export default function ChangeResultPage() {
   return <div>ChangeResultPage</div>;
--- src/features/pages/MainPage.tsx
+++ src/features/pages/MainPage.tsx
@@ -1,9 +1,11 @@
-import { Button } from '@/features/shared';
+import { Button, Card, Line } from '@/features/shared';
 
 export default function MainPage() {
   return (
-    <div>
+    <div className="flex flex-col px-4 pt-4 pb-12 gap-4">
+      <Card>test</Card>
       <Button>MainPage</Button>
+      <Line />
     </div>
   );
 }
