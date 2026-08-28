ceb8cbef7ef760e0b859f1cb3e990f4dbbc1a28a
donghyeon01 <85489985+donghyeon01@users.noreply.github.com>
2026-08-29 06:49:46 +0900

    feat: TanStack Query 도입 - QueryProvider 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './app/App.tsx';
+import { QueryProvider } from './app/QueryProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryProvider>
+      <App />
+    </QueryProvider>
   </StrictMode>,
 );
