e44953f55b463332cc4bcfe14229f287da6668d8
김혜진 <khj000217@naver.com>
2026-08-28 10:54:21 +0900

    refactor: 컴포넌트 구조 수정

--- src/features/coupon/components/EmptyCoupons.tsx
+++
@@ -1,7 +0,0 @@
-export default function EmptyCoupons() {
-  return (
-    <div className="flex min-h-60 items-center justify-center">
-      <p className="text-body text-fg-tertiary">검색 결과가 없어요.</p>
-    </div>
-  );
-}
